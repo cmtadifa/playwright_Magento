d3fe13d98c9fba3320041a2f18bedd1559fb2dc3
Carlos Miguel Tadifa <cdtadifa21@gmail.com>
2025-03-29 08:35:57 +0800

    push alignment for playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -17,20 +17,22 @@ export default defineConfig({
   testDir: './tests',
   testMatch: '**/*.js',
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 1 : 0,
+  retries: process.env.CI ? 1 : 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'html', // 'html', 'list', 'json', 'dot', 'line'
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://magento.softwaretestingboard.com/',
     actionTimeout: 30000,
+    // screenshot: 'on',
+    // video: 'on',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -73,6 +75,7 @@ export default defineConfig({
     // },
   ],
 
+  outputDir: 'test-results/',
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
